6b0ec4b4a6eaff8e3858c1a5abfc7790f95da190
FedrovSergey <serzh-fedorov-2004@mail.ru>
2021-09-17 16:37:14 +0300

    First try commit

---
+++ Laboratory1.cpp
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+
+int main() {
+	int x;
+	printf("enter number:");
+	scanf_s("%d", &x, "\n");
+	long long y = 0;
+	int i = 1;
+	int j = 10;
+	while (i <= 11) {
+		long long st = 1;
+		int n = j;
+		while (n > 0) {
+			st = st * x;
+			n--;
+		}
+		y = y + st * i;
+		i++;
+		j--;
+	}
+	printf("result:%d", y);
+	return 0;
+}
